c967630e26ff1f20c17e0bc69e2587710f167dcc
Amulya Paritosh <amulya1171@gmail.com>
2023-05-11 10:52:44 +0530

    tags creation

--- component_management_system/config.py
+++ component_management_system/config.py
@@ -8,8 +8,12 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'component_management.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+# with app.app_context():
+#     print("creating db")
+#     db.create_all()
--- component_management_system/tags/models.py
+++ component_management_system/tags/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from config import db
+from config import db, app
 from sqlalchemy.dialects.postgresql import UUID
 
 
@@ -10,3 +10,16 @@ class Tag(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     label = db.Column(db.String(32), unique=True)
     metadata_id = db.Column(db.Integer, db.ForeignKey("metadatas.id"), nullable=True)
+
+def init_db():
+
+    with app.app_context():
+        print("creating db")
+
+        new_tag = Tag(label="car")
+        db.session.add(new_tag)
+        db.session.commit()
+
+
+# if __name__ == '__main__':
+init_db()
