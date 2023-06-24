4b13164c7c20c0c2c7f64427a8c2b7c05641dbfb
Amulya Paritosh <amulya1171@gmail.com>
2023-06-25 03:08:58 +0530

    certificate added for elasticsearch

--- component_management_system/app/database/definations.py
+++ component_management_system/app/database/definations.py
@@ -1,13 +1,16 @@
 import os
 
+import certifi
 from elasticsearch import Elasticsearch
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
 ma = Marshmallow()
+
 es = Elasticsearch(
     "https://elasticsearch.localhost:9200/",
-    basic_auth=(os.environ.get("ELASTICSEARCH_USERNAME"), os.environ.get("ELASTICSEARCH_PASSWORD")),
-    verify_certs=False
+    basic_auth=(os.environ.get("ELASTICSEARCH_USERNAME", ""), os.environ.get("ELASTICSEARCH_PASSWORD", "")),
+    ca_certs=certifi.where(),
 )
--- component_management_system/config.py
+++ component_management_system/config.py
@@ -4,6 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    SECRET_KEY=os.environ.get("FLASK_SECRET_KEY")
+    DEBUG = True
+    TESTING = True
+
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/app.db"
-    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
