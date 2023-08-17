de81605e7f2bd8dfcca940b95eaec67c8852b70c
Amulya Paritosh <amulya1171@gmail.com>
2023-08-17 15:46:09 +0530

    added licenses

--- component_management_system.py
+++ component_management_system.py
@@ -1,5 +1,18 @@
 #!.venv/bin/python
 
+# SPDX-License-Identifier: MIT
+# --------------------------------------------------------------
+#|																|
+#|             Copyright 2023 - 2023, Amulya Paritosh			|
+#|																|
+#|  This file is part of Component Library Plugin for FreeCAD.	|
+#|																|
+#|               This file was created as a part of				|
+#|              Google Summer Of Code Program - 2023			|
+#|																|
+# --------------------------------------------------------------
+
+
 from dotenv import load_dotenv
 
 from src.app.main import create_app
