9a57f021b7e4946bf057f76c5899684f3270b624
Amulya Paritosh <amulya1171@gmail.com>
2023-08-21 12:47:18 +0530

    added type hint and docstring to config

--- src/config.py
+++ src/config.py
@@ -13,13 +13,35 @@
 
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir: str = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY=os.environ.get("FLASK_SECRET_KEY")
+    """
+    Configuration class for the Component Management System.
+
+    Attributes
+    ----------
+    SECRET_KEY : str | None
+        The secret key for the Flask application. Defaults to the value of the "FLASK_SECRET_KEY" environment variable.
+    DEBUG : bool
+        Flag indicating whether debug mode is enabled. Defaults to True.
+    TESTING : bool
+        Flag indicating whether testing mode is enabled. Defaults to True.
+    SQLALCHEMY_DATABASE_URI : str
+        The URI for the SQLAlchemy database connection. Defaults to a SQLite database located at "{basedir}/app.db".
+    SQLALCHEMY_TRACK_MODIFICATIONS : bool
+        Flag indicating whether SQLAlchemy should track modifications. Defaults to False.
+
+    Notes
+    -----
+    This class represents the configuration settings for the Component Management System.
+    It provides default values for various attributes used in the application.
+    """
+
+    SECRET_KEY: str | None = os.environ.get("FLASK_SECRET_KEY")
     DEBUG = True
     TESTING = True
 
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir}/app.db"
+    SQLALCHEMY_DATABASE_URI: str = f"sqlite:///{basedir}/app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
