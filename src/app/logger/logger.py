d8cf7f8e7c6ff0ba2d4ee1f82f31088d444c0c16
Amulya Paritosh <amulya1171@gmail.com>
2023-11-08 18:46:06 +0530

    disabled InsecureRequestWarning (elasticsearch)

--- src/app/logger/logger.py
+++ src/app/logger/logger.py
@@ -1,6 +1,11 @@
 import logging
+
+import urllib3
 from elasticsearch import logger as es_logger
-from .handlers import BaseStreamHandler, BaseRotatingFileHandler
+
+from .handlers import BaseRotatingFileHandler, BaseStreamHandler
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 es_logger.addHandler(BaseStreamHandler())
 es_logger.addHandler(BaseRotatingFileHandler())
