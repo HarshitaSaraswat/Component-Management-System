ce122fb11e62e8d69a1ca6c6289360eec5e37d7f
Amulya Paritosh <amulya1171@gmail.com>
2024-02-01 11:42:13 +0530

    Add authentication module for exchanging code for token

---
+++ src/app/authentication.py
@@ -0,0 +1,28 @@
+import requests
+
+from ..config import Config
+
+
+def exchange_code_for_token(code: str):
+    if not code:
+        raise ValueError("The authentication code has to be supplied!")
+
+    if Config.GITHUB_OAUTH_CLIENT_ID is None:
+        raise ValueError("The GitHub OAuth Client ID has to be supplied!")
+
+    if Config.GITHUB_OAUTH_CLIENT_SECRET is None:
+        raise ValueError("The GitHub OAuth Client Secret has to be supplied!")
+
+    response = requests.post(
+        "https://github.com/login/oauth/access_token",
+        data={
+            "client_id": Config.GITHUB_OAUTH_CLIENT_ID,
+            "client_secret": Config.GITHUB_OAUTH_CLIENT_SECRET,
+            "code": code,
+        },
+        headers={"Accept": "application/json"},
+        timeout=10,
+    )
+
+    if response.status_code == 200:
+        return response.json().get("access_token")
