5cfcd3921bffd3ecafa57429609f8a4ac8dd6002
Amulya Paritosh <amulya1171@gmail.com>
2024-03-05 15:39:49 +0530

    Update authentication middleware to use GitHub OAuth

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-from .authentication_middleware import exchange_code_for_token
+from ..authentication.github_OAuth import exchange_code_for_token
 
 
 def create_routes(app: Flask):
