822035f3a1f91ddd5d14548f6a74a8726e7e28dc
Amulya Paritosh <amulya1171@gmail.com>
2024-09-02 22:28:35 +0530

    added auth methods to swagger file with new endpoints

--- src/app/routes.py
+++ src/app/routes.py
@@ -36,17 +36,17 @@ def create_routes(app: Flask):
         if not access_token:
             return "No access token received", 400
 
-        user_data = get_github_user(access_token)
-        user = User.query.filter_by(username=user_data["login"]).first()
+    #     user_data = get_github_user(access_token)
+    #     user = User.query.filter_by(username=user_data["login"]).first()
 
-        if not user:
-            user = User(username=user_data["login"], admin=False)
-            user.create()
-        jwt = encode_auth_token(str(user.id))
-        logger.debug(f"{jwt=}")
-        return {
-            "jwt": jwt,
-        }, 200
+    #     if not user:
+    #         user = User(username=user_data["login"], admin=False)
+    #         user.create()
+    #     jwt = encode_auth_token(str(user.id))
+    #     logger.debug(f"{jwt=}")
+    #     return {
+    #         "jwt": jwt,
+    #     }, 200
 
 =======
 >>>>>>> cec9cd2 (Update dependencies and add logging and authentication routes)
@@ -62,16 +62,16 @@ def create_routes(app: Flask):
 >>>>>>> cec9cd2 (Update dependencies and add logging and authentication routes)
         auth_token = exchange_code_for_token(code)
 
-        if auth_token is None:
-            return "Could not authenticate with GitHub", 400
+    #     if auth_token is None:
+    #         return "Could not authenticate with GitHub", 400
 
 <<<<<<< HEAD
         user_data = get_github_user(auth_token)
         user = User.query.filter_by(username=user_data["login"]).first()
 
-        if not user:
-            user = User(username=user_data["login"], admin=False)
-            user.create()
+    #     if not user:
+    #         user = User(username=user_data["login"], admin=False)
+    #         user.create()
 
         return {
             "github_auth_token": auth_token,
